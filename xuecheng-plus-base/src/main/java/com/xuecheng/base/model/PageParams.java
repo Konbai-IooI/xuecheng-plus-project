5c79efbfec7753c7c5c843164cfd6fa793046f05
IooI <3217018143@qq.com>
2023-11-03 21:01:28 +0800

    22

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -14,14 +14,15 @@ import lombok.ToString;
 @ToString
 public class PageParams {
 
-    //当前页码
+    // 当前页码
     @ApiModelProperty("页码")
     private Long pageNo = 1L;
-    //每页显示记录数
+    // 每页显示记录数
     @ApiModelProperty("每页记录数")
     private Long pageSize = 30L;
 
     public PageParams() {
+
     }
 
     public PageParams(Long pageNo, Long pageSize) {
