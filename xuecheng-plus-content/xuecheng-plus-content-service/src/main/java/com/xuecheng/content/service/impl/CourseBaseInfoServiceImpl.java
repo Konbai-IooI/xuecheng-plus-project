b1ec5066e1ac89e05d1fb6b8b903eb24dd83879d
iooi <321@qq.com>
2023-12-17 21:12:29 +0800

    新增课程

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -144,6 +144,7 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
             BeanUtils.copyProperties(courseMarketNew, courseMarket);
             // 更新
             return courseMarketMapper.updateById(courseMarket);
+
         }
     }
 }
