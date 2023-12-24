790ac2a38ff380968ada5af40d83440d516a1ee9
Mona <321@qq.com>
2023-12-24 22:27:17 +0800

    Gateway

---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * @author iooi
+ * description Gateway
+ * @data 2023/12/24 21:11
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
